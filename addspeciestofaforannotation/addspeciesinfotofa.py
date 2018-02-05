03e1572e630659c5e2826a5b6151e6b409dfd333
bladrome <blackwhitedoggie@163.com>
2018-02-05 23:46:43 +0800

    Maybe a name could be map to many species

--- addspeciestofaforannotation/addspeciesinfotofa.py
+++ addspeciestofaforannotation/addspeciesinfotofa.py
@@ -17,18 +17,25 @@ def readphylumfile(phylumfilepath):
             x[0] = "_".join(x[0].strip("\"").split())
             value = x[0]
             key = x[1:]
-            phylumdict.update({key[i]: value for i in range(len(key))})
+            species = {key[i]: value for i in range(len(key))}
+
+            for key, value in species.items():
+                if key in phylumdict:
+                    phylumdict[key].append(value)
+                else:
+                    phylumdict[key] = [value]
 
     return phylumdict
 
 
-def output():
+# def output():
 
-    fa = readfafile("./11-L_HFGMYALXX_L3.unigene.fa")
-    phylum = readphylumfile("./11-L_HFGMYALXX_L3.pyhum_1")
+fa = readfafile("./11-L_HFGMYALXX_L3.unigene.fa")
+phylum = readphylumfile("./11-L_HFGMYALXX_L3.pyhum_1")
 
-    for name, seq in fa.items():
-        if name in phylum:
-            print("_".join((phylum[name], name)) + "\n" + seq)
+for name, seq in fa.items():
+    if name in phylum:
+        for species in phylum[name]:
+            print("_".join((species, name)) + "\n" + seq)
 
-output()
+# output()
