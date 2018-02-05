abf298bbcad4bc13d1bdc970c289c3d676a32f6a
bladrome <blackwhitedoggie@163.com>
2018-02-05 14:03:24 +0800

    Add normalize aboundance matrix by columns

---
+++ normalizeaboundancebycolumns/probobilize.py
@@ -0,0 +1,25 @@
+import numpy as np
+import csv
+
+header = ""
+def readdata(filepath):
+    global header
+    samples = list()
+    with open(filepath) as csvfile:
+        header = csvfile.readline().strip()
+        csvreader = csv.reader(csvfile, delimiter=',')
+        for line in csvreader:
+            samples.append(line)
+    return samples
+
+data = np.array(readdata("./data.csv"))
+varx = np.array(data[:, 1:], dtype=float)
+varsum = np.sum(varx, axis=0)
+res = varx / varsum
+
+res = np.column_stack((data[:, 0].reshape(10, 1), res))
+
+print(header)
+for i in res:
+    print(",".join(i))
+
