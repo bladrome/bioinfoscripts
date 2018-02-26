2784ef9638c24df4821803d6fa3d558383203d39
bladrome <blackwhitedoggie@163.com>
2018-02-26 15:01:22 +0800

    Add booleananalysis/test.py

--- booleananalysis/test.py
+++ booleananalysis/test.py
@@ -1,7 +1,6 @@
 import esabomethod
 import spearman
 
-
 ep = esabomethod.esabopositive
 en = esabomethod.esabonegative
 
@@ -13,7 +12,6 @@ print("EP:" + str(len(ep)))
 print("EN:" + str(len(en)))
 print("SP:" + str(len(sp)))
 print("SN:" + str(len(sn)))
-
 
 print(ep.intersection(sp))
 print(en.intersection(sn))
