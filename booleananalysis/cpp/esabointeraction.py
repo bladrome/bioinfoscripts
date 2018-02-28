4d1162267f2b150b580341c85e351d5e78c98fba
bladrome <blackwhitedoggie@163.com>
2018-02-28 15:57:14 +0800

    Extract useful funtion in booleananalysis/cpp/esabointeraction.py

--- booleananalysis/cpp/esabointeraction.py
+++ booleananalysis/cpp/esabointeraction.py
@@ -2,21 +2,26 @@ import sys
 import numpy as np
 import pandas as pd
 
-if len(sys.argv) != 2:
-    print("Usage: {0} {1}".format(sys.argv[0], "threhold"))
-    sys.exit(1)
 
-threhold = float(sys.argv[1])
+def esabo_interaction(csvfile, threhold):
+    data = pd.read_csv(csvfile, header=None)
 
-data = pd.read_csv("result.csv", header=None)
+    pos = np.where(data[data > threhold] > 0)
+    neg = np.where(data[data < -threhold] < 0)
 
-pos = np.where(data[data > threhold] > 0)
-neg = np.where(data[data < -threhold] < 0)
+    posset = set(zip(pos[0], pos[1]))
+    negset = set(zip(neg[0], neg[1]))
 
-posset = set(zip(pos[0], pos[1]))
-negset = set(zip(neg[0], neg[1]))
+    # print("Positive interaction")
+    # print(posset)
+    # print("Negative interaction")
+    # print(negset)
+    return posset, negset
 
-print("Positive interaction")
-print(posset)
-print("Negative interaction")
-print(negset)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {0} {1}".format(sys.argv[0], "threhold"))
+        sys.exit(1)
+    threhold = float(sys.argv[1])
+    esabo_interaction("./result.csv", threhold)
