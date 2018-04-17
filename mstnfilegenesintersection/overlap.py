403cc3838549d53780cd54717bf5747e9f7a1e23
bladrome <blackwhitedoggie@163.com>
2018-04-17 15:45:13 +0800

    Change sereies type in mstnfilegenesintersection/overlap.py

--- mstnfilegenesintersection/overlap.py
+++ mstnfilegenesintersection/overlap.py
@@ -1,18 +1,18 @@
 import sys
 
-import numpy as np
-import pandas as pd
-
 if len(sys.argv) != 4:
     print("Usage: {0} {1} {2} {3}".format(sys.argv[0], "MSTN_file1.txt",
                                           "MSTN_file2.txt", "output.csv"))
     sys.exit(1)
 
+import numpy as np
+import pandas as pd
 a = pd.read_table(sys.argv[1], header=None)
 a = a.sort_values(by=[1])
 a['a_end'] = a[2] + 23
 a = a.rename(columns={1: 'chromosome'})
 a = a.rename(columns={2: 'a_start'})
+a.chromosome = a.chromosome.astype(str)
 
 b = pd.read_table(sys.argv[2])
 b['chromosome'] = [i.split(':')[0] for i in b['# Location']]
@@ -20,6 +20,7 @@ b['b_start'] = [i.split(':')[1].split('-')[0] for i in b['# Location']]
 b['b_end'] = [i.split(':')[1].split('-')[1] for i in b['# Location']]
 b.b_start = b.b_start.astype(int)
 b.b_end = b.b_end.astype(int)
+b.chromosome = b.chromosome.astype(str)
 
 c = a.merge(b, on=['chromosome'])
 
