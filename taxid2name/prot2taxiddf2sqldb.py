072787ce7d5b6069ac606b64d7c113b31cf9898e
bladrome <blackwhitedoggie@163.com>
2018-08-09 10:34:07 +0800

    Chunk big file

--- taxid2name/prot2taxiddf2sqldb.py
+++ taxid2name/prot2taxiddf2sqldb.py
@@ -1,8 +1,18 @@
 import sqlite3
 import pandas as pd
 conn = sqlite3.connect("protaccession2taxid.db")
-prot2taxiddf = pd.read_table("./prot.accession2taxid10000")
-prot2taxiddf.to_sql("PROTACCESSION2TAXID", conn, if_exists="append", index=False)
+
+prot2taxiddf = pd.read_table("./prot.accession2taxid10000", iterator=True)
+
+while True:
+    try:
+        chunk = prot2taxiddf.get_chunk(10)
+        print(chunk)
+        chunk.to_sql("PROTACCESSION2TAXID", conn, if_exists="append", index=False)
+        print("Looping")
+    except StopIteration:
+        break
+
 for i in conn.execute("SELECT DISTINCT * FROM PROTACCESSION2TAXID WHERE gi=1168978"):
     print(i)
 
