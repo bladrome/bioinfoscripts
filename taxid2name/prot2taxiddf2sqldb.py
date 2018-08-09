a0ff860c4537d8de81ee11d3a784d3d3543af839
bladrome <blackwhitedoggie@163.com>
2018-08-09 16:43:53 +0800

    iterator csv, chunksize is of greate importance

--- taxid2name/prot2taxiddf2sqldb.py
+++ taxid2name/prot2taxiddf2sqldb.py
@@ -1,18 +1,40 @@
-import sqlite3
 import pandas as pd
-conn = sqlite3.connect("protaccession2taxid.db")
+import sqlite3
 
-prot2taxiddf = pd.read_table("./prot.accession2taxid10000", iterator=True)
 
-while True:
-    try:
-        chunk = prot2taxiddf.get_chunk(10)
-        print(chunk)
-        chunk.to_sql("PROTACCESSION2TAXID", conn, if_exists="append", index=False)
-        print("Looping")
-    except StopIteration:
-        break
+prot2taxifile = "./prot.accession2taxid10000"
+def build_protacc2taxid(prot2taxidfile):
+    conn = sqlite3.connect("protaccession2taxid.db")
+    prot2taxiddf = pd.read_table(prot2taxidfile, iterator=True)
 
-for i in conn.execute("SELECT DISTINCT * FROM PROTACCESSION2TAXID WHERE gi=1168978"):
-    print(i)
+    while True:
+        try:
+            chunk = prot2taxiddf.get_chunk(10)
+            print(len(chunk))
+            chunk.to_sql("PROTACCESSION2TAXID", conn, if_exists="append", index=False)
+        except StopIteration:
+            break
 
+taxid2namefile = "./names.dmp"
+def build_taxid2name(taxid2namefile):
+    conn = sqlite3.connect("taxid2name.db")
+    # prot2taxiddf = pd.read_table(taxid2namefile, iterator=True)
+    taxi2dnamesdf = pd.read_table(taxid2namefile, sep='\t',header=None, usecols=[0, 2, 4, 6], iterator=True)
+    taxi2dnamesdf.columns = ["tax_id", "name_txt", "unique_name", "name"]
+    print(taxi2dnamesdf.columns)
+
+    while True:
+        try:
+            chunk = taxi2dnamesdf.get_chunk(200000)
+            print(chunk.columns)
+            print(len(chunk))
+            chunk.to_sql("TAXID2NAMES", conn, if_exists="append", index=False)
+        except StopIteration:
+            break
+
+# test
+# for i in conn.execute("SELECT DISTINCT * FROM PROTACCESSION2TAXID WHERE gi=1168978"):
+    # print(i)
+
+
+build_taxid2name(taxid2namefile)
