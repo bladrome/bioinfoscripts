6dcd9038c8a0154aec62ff58114c894c95cf899e
bladrome <blackwhitedoggie@163.com>
2018-08-09 17:46:10 +0800

    blast results -> gi

--- taxid2name/gi2taxid2taxnames.py
+++ taxid2name/gi2taxid2taxnames.py
@@ -1,20 +1,35 @@
-import pandas as pd
 import sqlite3
+from os import popen
+
+import pandas as pd
+
 from ete3 import NCBITaxa
 
 ncbi = NCBITaxa()
-
-gilist = [ 1168978, 116525, 116527, 17374148, 21903391, 116530, 116531, 1705937, 116533 ]
-
 gi2idconn = sqlite3.connect("protaccession2taxid.db")
 id2naconn = sqlite3.connect("taxid2name.db")
 
-for gi in gilist:
-    for i in gi2idconn.execute("SELECT DISTINCT * FROM PROTACCESSION2TAXID WHERE gi={0}".format(gi)):
-        taxid = i[2]
-        for j in id2naconn.execute("SELECT \"0\", \"2\" FROM TAXID2NAMES WHERE \"0\"={0}".format(taxid)):
-            taxid = j[0]
-            name = j[1]
-            rank = ncbi.get_rank([taxid])[taxid]
-            print(",".join((str(gi), str(taxid), name, rank)))
-            break
+gilist = [
+    1168978, 116525, 116527, 17374148, 21903391, 116530, 116531, 1705937,
+    116533
+]
+
+pcmd = "cat all_protein_protein.blast | cut -d '\t' -f 1-3"
+
+with popen(pcmd) as f:
+    for line in f:
+        line = line.split()
+        gene = line[0]
+        gi = line[2].split("|")[1]
+        for i in gi2idconn.execute(
+                "SELECT DISTINCT * FROM PROTACCESSION2TAXID WHERE gi={0}".
+                format(gi)):
+            taxid = i[2]
+            for j in id2naconn.execute(
+                    "SELECT \"0\", \"2\" FROM TAXID2NAMES WHERE \"0\"={0}".
+                    format(taxid)):
+                taxid = j[0]
+                name = j[1]
+                rank = ncbi.get_rank([taxid])[taxid]
+                print(",".join((gene, str(gi), str(taxid), name, rank)))
+                break
