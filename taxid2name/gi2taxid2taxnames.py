50f5885431a8871110cd8a1e9af8f86c64bc5474
bladrome <blackwhitedoggie@163.com>
2018-08-09 16:53:51 +0800

    Add gi output

--- taxid2name/gi2taxid2taxnames.py
+++ taxid2name/gi2taxid2taxnames.py
@@ -4,17 +4,17 @@ from ete3 import NCBITaxa
 
 ncbi = NCBITaxa()
 
-gilist = pd.read_table("./gi_list")
+gilist = [ 1168978, 116525, 116527, 17374148, 21903391, 116530, 116531, 1705937, 116533 ]
 
 gi2idconn = sqlite3.connect("protaccession2taxid.db")
 id2naconn = sqlite3.connect("taxid2name.db")
 
-for gi in gilist.itertuples(index=False):
-    for i in gi2idconn.execute("SELECT DISTINCT * FROM PROTACCESSION2TAXID WHERE gi={0}".format(gi[0])):
+for gi in gilist:
+    for i in gi2idconn.execute("SELECT DISTINCT * FROM PROTACCESSION2TAXID WHERE gi={0}".format(gi)):
         taxid = i[2]
         for j in id2naconn.execute("SELECT \"0\", \"2\" FROM TAXID2NAMES WHERE \"0\"={0}".format(taxid)):
             taxid = j[0]
             name = j[1]
             rank = ncbi.get_rank([taxid])[taxid]
-            print(",".join((str(taxid), name, rank)))
+            print(",".join((str(gi), str(taxid), name, rank)))
             break
