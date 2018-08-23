9731b1ea16e4758090219941f305c793a94cce93
bladrome <blackwhitedoggie@163.com>
2018-08-23 21:33:18 +0800

    Add test data

--- vcfsamtoolsfilter/vcfsamtoolsfilter.py
+++ vcfsamtoolsfilter/vcfsamtoolsfilter.py
@@ -2,12 +2,13 @@ from os import popen
 
 import pandas as pd
 
-samtoolsvcffile = "./family123.samtools.mpileup.vcf"
+# samtoolsvcffile = "./family123.samtools.mpileup.vcf"
+samtoolsvcffile = "./samtools.vcf"
 cmdstr = 'cat ' + samtoolsvcffile + \
     '| grep -v "#" | awk -v OFS="\t" \'{ split($10, F, ":"); split($11, M, ":"); split($12, f, ":"); print($0,F[1],M[1], f[1], F[1]","M[1]","f[1])}\''
 
 # print(cmdstr)
-vcfdf = pd.read_table(popen(cmdstr, 'r'), header=None, nrows=10000)
+vcfdf = pd.read_table(popen(cmdstr, 'r'), header=None)#, nrows=10000)
 vcfdf.columns = [
     "CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT",
     "Father", "Mother", "f", "F_GT", "M_GT", "f_GT", "GT"
