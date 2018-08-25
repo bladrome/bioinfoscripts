c34b45556be1316e75dc60444219ee90cbc6dc1f
bladrome <blackwhitedoggie@163.com>
2018-08-25 16:51:30 +0800

    Add -o output option

--- statsfasta/statsFastaNx0.py
+++ statsfasta/statsFastaNx0.py
@@ -20,9 +20,11 @@ if __name__ == "__main__":
         "-o",
         "--output",
         dest="outputfile",
-        type=argparse.FileType('w'))
+        type=argparse.FileType('w'),
+        default=sys.stdout)
     args = parser.parse_args()
     lenthreshold = args.len_threshold
+    outputfile = args.outputfile
     # fastafilepath = "/media/bladrome/drome/Biodata/contigs/11-hui_HFGMYALXX_L4_clean.contigs.fa"
     import pandas as pd
     from Bio import SeqIO
@@ -32,16 +34,16 @@ if __name__ == "__main__":
     lensum = length_cuts.sum()
     cumsum = length_cuts.cumsum()
     Nx0 = cumsum / lensum
-    print("-" * 30)
-    print("Summary:")
-    print("Sum length:\t{0}".format(lensum))
-    print("# of seq:\t{0}".format(len(length_cuts)))
-    print("Avg length:\t{0}".format(lensum / len(length_cuts)))
-    print(length_cuts.describe())
-    print("-" * 30)
-    print("Nx0")
+    print("-" * 30, file=outputfile)
+    print("Summary:", file=outputfile)
+    print("Sum length:\t{0}".format(lensum), file=outputfile)
+    print("# of seq:\t{0}".format(len(length_cuts)), file=outputfile)
+    print("Avg length:\t{0}".format(lensum / len(length_cuts)), file=outputfile)
+    print(length_cuts.describe(), file=outputfile)
+    print("-" * 30, file=outputfile)
+    print("Nx0", file=outputfile)
     for n in pd.np.arange(0.0, 1.1, 0.05):
         for item in Nx0.iteritems():
             if item[1] >= n:
-                print("N{0}\t{1}".format(int(n * 100), length_cuts[item[0]]))
+                print("N{0}\t{1}".format(int(n * 100), length_cuts[item[0]]), file=outputfile)
                 break
